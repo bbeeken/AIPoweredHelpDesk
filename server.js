03a76e92fa48dbd9eafddd057857af32b0a7bf54
bbeeken <41312445+bbeeken@users.noreply.github.com>
2025-07-11 01:37:42 -0500

    Add CORS support

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const data = require('./data/mockData');
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    return res.end();
+  }
+  next();
+});
+
 
 // Basic health check endpoint
 app.get('/health', (req, res) => {
