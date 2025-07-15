707e54d52e19004194673ba28226d25850068416
bbeeken <41312445+bbeeken@users.noreply.github.com>
2025-07-15 12:19:24 -0500

    remove token auth

--- tests/authHeader.js
+++ tests/authHeader.js
@@ -1,14 +1,11 @@
 const http = require('http');
-const auth = require('../utils/authService');
 
-const { token } = auth.authenticate('Brian', 'password1');
-const authHeader = { Authorization: `Bearer ${token}` };
 
 function wrap(fn) {
   return function (options, ...args) {
     if (typeof options === 'string') options = new URL(options);
     else options = { ...options };
-    options.headers = { ...authHeader, ...(options.headers || {}) };
+    options.headers = { ...(options.headers || {}) };
     return fn.call(http, options, ...args);
   };
 }
@@ -16,4 +13,4 @@ function wrap(fn) {
 http.request = wrap(http.request);
 http.get = wrap(http.get);
 
-module.exports = { token, authHeader };
+module.exports = {};
